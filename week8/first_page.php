ac17f57eef95c4ac562b92741ab39ef609564e4e
Maryrose Pergis <mrpergis@student.apc.edu.ph>
2024-01-26 22:10:51 +0800

    PHP Lesson 3: Echo/Print

--- week8/first_page.php
+++ week8/first_page.php
@@ -9,7 +9,9 @@
 </head>
 <body>
     
-    <canvas id="canvas" style="background: #111;"></canvas>
+    <?php
+    echo "Maryrose Pergis was here!";
+    ?>
 
     <div class="first_page">
         <h1 class="big_font">Hello and welcome, <span class="hacker-text" data-value="USER">USER</span></h1>
@@ -18,7 +20,46 @@
     <div class="button">
         <a href='index.php'><b>Proceed</b></a>
     </div>
-    <script src="script.js"></script>
+    <script>
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+
+class HackerText {
+    constructor(element) {
+        this.element = element;
+        this.iterations = 0;
+        this.startAnimation();
+    }
+
+    startAnimation() {
+        const interval = setInterval(() => {
+            this.element.innerText = this.element.innerText.split("")
+                .map((letter, index) => {
+                    if (index < this.iterations) {
+                        return this.element.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random() * 26)];
+                })
+                .join("");
+
+            if (this.iterations >= this.element.dataset.value.length) {
+                clearInterval(interval);
+                setTimeout(() => {
+                    this.iterations = 0;
+                    this.startAnimation();
+                }, 1000);
+            }
+
+            this.iterations += 1;
+        }, 30);
+    }
+}
+
+// Apply the class to each element with the "hacker-text" class
+const hackerTextElements = document.querySelectorAll('.hacker-text');
+hackerTextElements.forEach(element => new HackerText(element));
+
+    </script>
     
 </body>
 </html>
