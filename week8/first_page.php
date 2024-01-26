4f1fdce6e4f4e958207815542fb13e772fbc10b7
Maryrose Pergis <mrpergis@student.apc.edu.ph>
2024-01-26 15:09:54 +0800

    Moved Week 5 to Week 8

---
+++ week8/first_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Welcome, user.</title>
+</head>
+<body>
+    
+    <canvas id="canvas" style="background: #111;"></canvas>
+
+    <div class="first_page">
+        <h1 class="big_font">Hello and welcome, <span class="hacker-text" data-value="USER">USER</span></h1>
+    </div>
+    <br><br>
+    <div class="button">
+        <a href='index.php'><b>Proceed</b></a>
+    </div>
+    <script src="script.js"></script>
+    
+</body>
+</html>
+
